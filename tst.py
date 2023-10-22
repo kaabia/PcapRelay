c42f702bda9f9fa4eb711972cab35a43ca8065fb
Badr Bacem KAABIA <badr.bacem.kaabia-EXT@conti-engineering.com>
2023-10-22 19:18:06 +0200

    Add pylint configuration and enhancements

--- tst.py
+++ tst.py
@@ -1,6 +1,42 @@
-import argparse
+'''
+_______________________________________________________________________________#
+
+ File    : tst.py
+ Author  : Badr Bacem KAABIA
+ Version : 0.1
+ Date    : 22 October 2023
+ Brief   : test script
+_______________________________________________________________________________
+
+MIT License
+
+Copyright (c) 2023 Badr Bacem KAABIA
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+_______________________________________________________________________________#
+'''
+from __future__ import absolute_import
+from argparse import ArgumentParser
 import os
-parser = argparse.ArgumentParser()
+
+parser = ArgumentParser()
 
 parser.add_argument("-i",
                     "--interface",
